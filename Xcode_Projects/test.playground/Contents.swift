b808695685876aafc06c2c11ed6a73a37e9a90a2
aespidol <aespidol@gmail.com>
2016-04-04 11:21:36 -0700

    first commit

---
+++ Xcode_Projects/test.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let quiz = [("This","and"), ("What","ever")]
+
+print(quiz.count)
+arc4random_uniform(UInt32(quiz.count))
+
+var name = ["Andrew", "Alex", "Jake","Alfred","Mila", "Linh", "Vanessa", "Kaya"]
+
+var nameIndex = arc4random_uniform(UInt32(name.count))
+
+print(5-5-5)
+
+name[1] = "Cory"
+print(name)
+
+var x = "1"
+var y = "2"
+var z = "."
+var a = "0"
+print(x+y+z+a)
+
+var change = Double(x+y+z+a)
+print(change)
